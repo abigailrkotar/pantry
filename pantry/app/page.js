4e11b0df0b14f6072aa7d5d363f7aa26a940cd90
Abigail Kotar <abbyrosekotar@gmail.com>
2024-07-29 12:30:44 -0400

    fixed up list

--- pantry/app/page.js
+++ pantry/app/page.js
@@ -1,4 +1,4 @@
-import {Box, Stack} from '@mui/material'
+import {Box, Stack, Typography} from '@mui/material'
 
 const item = [
   'tomato',
@@ -20,10 +20,14 @@ export default function Home() {
       width="100vw" 
       height="100vh" 
       display={"flex"}
+      flexDirection={'column'}
       justifyContent={"center"}
       alignItems={"center"}
     >
-      <Stack width="800px" height="300px" spacing={2} overflow={'scroll'}>
+    <Box width="800px" height="100px" bgcolor={'lightblue'} border={'1px solid #333'}
+    ><Typography variant={'h2'} color={'#333'} textAlign={'center'}>
+      Pantry Items</Typography></Box>
+      <Stack width="800px" height="300px" spacing={2} overflow={'auto'}>
 
         {item.map((i) => (
           <Box
@@ -33,8 +37,17 @@ export default function Home() {
             display={'flex'}
             justifyContent={'center'}
             alignItems={'center'}
-            bgcolor={'lightblue'}>
-              {i}
+            bgcolor={'#f0f0f0'}>
+              <Typography
+              variant={'h3'}
+              color={'#333'}
+              textAlign={'center'}>
+                {
+                  //capitalize the first letter
+                  i.charAt(0).toUpperCase() + i.slice(1)
+                }
+              </Typography>
+              
             </Box>
         ))}
       </Stack>
